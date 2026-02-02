802f4d157ea8873f902ae540fb36d3ad522432f6
Austine0829 <audreytuazon29@gmail.com>
2026-02-02 20:21:18 +0800

    (add) homepage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../sections/acoustic.dart';
+import '../sections/chaotic.dart';
+import '../sections/chill_vibe.dart';
+import '../sections/energetic.dart';
+import '../sections/home_banner.dart';
+import '../sections/mix.dart';
+import '../sections/recent_tracks.dart';
+import '../sections/road_trip.dart';
+import '../sections/top_listen.dart';
+import '../utils/app_text_theme.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home", style: Theme.of(context).textTheme.pageLabel),
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 17),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              HomeBanner(),
+              SizedBox(height: 10),
+              RoadTripSection(),
+              RecentTracksSection(),
+              EnergeticSection(),
+              ChaoticSection(),
+              MixSection(),
+              AcousticSection(),
+              TopListenSection(),
+              ChillVibeSection(),
+              SizedBox(height: 50)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
