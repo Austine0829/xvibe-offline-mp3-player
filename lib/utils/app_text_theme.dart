920043346314e2a9984c93f7679638dd4505bd6c
Austine0829 <audreytuazon29@gmail.com>
2026-02-04 05:50:38 +0800

    (update) font sizes

--- lib/utils/app_text_theme.dart
+++ lib/utils/app_text_theme.dart
@@ -6,20 +6,21 @@ extension AppTextTheme on TextTheme {
                          color: Colors.white);
 
   TextStyle get sectionLabel => 
-    titleLarge!.copyWith(fontSize: 22,
+    titleLarge!.copyWith(fontSize: 21,
                          color: Colors.white,);
 
   TextStyle get horizontalCardTitle => 
-    titleLarge!.copyWith(fontSize: 14,
+    titleLarge!.copyWith(fontSize: 13,
                          color: Colors.white);
 
   TextStyle get cardGenre => 
-    titleLarge!.copyWith(fontSize: 14,
+    titleLarge!.copyWith(fontSize: 13,
                          color: Colors.white54);
 
   TextStyle get verticalCardTitle => 
-    titleLarge!.copyWith(fontSize: 14,
+    titleLarge!.copyWith(fontSize: 13,
                          color: Colors.white);  
+                         
   TextStyle get textButtonColor => 
     titleLarge!.copyWith(color: Colors.white54);                      
 }
