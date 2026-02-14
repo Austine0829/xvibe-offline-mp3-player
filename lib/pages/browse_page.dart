0e14bf4416b1fe7f7e35fb4963578cdbd3181b54
Austine0829 <audreytuazon29@gmail.com>
2026-02-14 10:37:22 +0800

    (add) browse page

---
+++ lib/pages/browse_page.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:xvibe_offline_mp3_player/utils/app_text_theme.dart';
+import 'package:xvibe_offline_mp3_player/widgets/browse_catalog_grid_view.dart';
+import 'package:xvibe_offline_mp3_player/widgets/browse_search_song_list_view.dart';
+
+class BrowsePage extends StatefulWidget {
+  const BrowsePage({super.key});
+
+  @override
+  State<BrowsePage> createState() => _BrowsePageState();
+}
+
+class _BrowsePageState extends State<BrowsePage> {
+  ValueNotifier<String> text = ValueNotifier('');
+  Timer? _debounce;
+
+  @override
+  void dispose() {
+    _debounce!.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("Browse", style: Theme.of(context).textTheme.pageLabel),
+          backgroundColor: Colors.black,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  onChanged: (value) {
+                    if (_debounce?.isActive ?? false) _debounce!.cancel();
+
+                    _debounce = Timer(const Duration(milliseconds: 500), () {
+                      text.value = value;
+                    });
+                  },
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.search),
+                    hintText: "What do you want to listen to?",
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                ValueListenableBuilder(
+                  valueListenable: text,
+                  builder: (_, value, _) {
+                    return value.isEmpty
+                        ? BrowseCatalogGridView()
+                        : BrowseSearchSongListView();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
