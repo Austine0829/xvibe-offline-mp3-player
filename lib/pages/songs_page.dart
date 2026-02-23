330dc58c03e2b2697d079b61e2c7e3dacbbd7e68
Austine0829 <audreytuazon29@gmail.com>
2026-02-23 21:26:47 +0800

    (add) song list page

--- lib/pages/songs_page.dart
+++ lib/pages/songs_page.dart
@@ -1,18 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:xvibe_offline_mp3_player/utils/app_text_theme.dart';
+import 'package:xvibe_offline_mp3_player/widgets/shared/horizontal_song_card.dart';
 
 class SongsPage extends StatefulWidget {
-  const SongsPage({super.key});
+  final String pageLabel;
+
+  const SongsPage({super.key, required this.pageLabel});
 
   @override
   State<SongsPage> createState() => _SongsPageState();
 }
 
 class _SongsPageState extends State<SongsPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Row(
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: Icon(Icons.arrow_back, color: Colors.white),
+            ),
+          ],
+        ),
+        automaticallyImplyLeading: false,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 0.5 * MediaQuery.of(context).size.height,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/music_card_default.jpeg"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: AlignmentGeometry.bottomCenter,
+                    child: SizedBox(
+                      height: 0.03 * MediaQuery.of(context).size.height,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: 0.8),
+                              blurRadius: 10,
+                              offset: Offset(-5, 10),
+                            ),
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: .8),
+                              blurRadius: 10,
+                              offset: Offset(10, 10),
+                            ),
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: 0.5),
+                              blurRadius: 10,
+                              offset: Offset(-5, -5),
+                            ),
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: 0.5),
+                              blurRadius: 10,
+                              offset: Offset(10, -5),
+                            ),
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: 0.3),
+                              blurRadius: 15,
+                              offset: Offset(-5, -19),
+                            ),
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: 0.3),
+                              blurRadius: 15,
+                              offset: Offset(5, -19),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 10
+              ),
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      minimumSize: Size(50, 50),
+                      padding: EdgeInsets.zero,
+                    ),
+                    onPressed: () {},
+                    child: Icon(Icons.play_arrow, size: 30, color: Colors.black),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.shuffle, color: Colors.grey, size: 30),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  children: [
+                    ...List.generate(50, (index) {
+                        return Column(
+                          children: [
+                            HorizontalSongCard(
+                              songTitle: "Look at me", 
+                              songVibe: "Chill"
+                          ),
+                          SizedBox(height: 10)
+                        ],
+                      );
+                    })
+                  ],
+                )
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
