5ecd9c4d0b2dfff748ced9a5c149aed26ed66f86
Austine0829 <audreytuazon29@gmail.com>
2026-03-02 16:19:11 +0800

    (update) removed unused var and renamed the widget

--- lib/pages/show_more_page.dart
+++ lib/pages/show_more_page.dart
@@ -2,23 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:xvibe_offline_mp3_player/models/song.dart';
 import 'package:xvibe_offline_mp3_player/widgets/shared/horizontal_song_card.dart';
 
-class SongsPage extends StatefulWidget {
-  final String pageLabel;
+class ShowMorePage extends StatefulWidget {
   final String playlistId;
   final List<Song> songs;
 
-  const SongsPage({
+  const ShowMorePage({
     super.key, 
-    required this.pageLabel,
     required this.songs,
     required this.playlistId
   });
 
   @override
-  State<SongsPage> createState() => _SongsPageState();
+  State<ShowMorePage> createState() => _ShowMorePageState();
 }
 
-class _SongsPageState extends State<SongsPage> {
+class _ShowMorePageState extends State<ShowMorePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
