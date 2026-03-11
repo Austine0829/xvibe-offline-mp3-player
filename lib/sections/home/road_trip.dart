ea5120aa2347c48117d76c20e01196a01aed3e5b
Austine0829 <audreytuazon29@gmail.com>
2026-03-12 06:46:35 +0800

    (add) mock data and the playlist service to test music player

--- lib/sections/home/road_trip.dart
+++ lib/sections/home/road_trip.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:xvibe_offline_mp3_player/constants/label_name.dart';
-import 'package:xvibe_offline_mp3_player/pages/songs_page.dart';
+import 'package:xvibe_offline_mp3_player/constants/playlist_id.dart';
+import 'package:xvibe_offline_mp3_player/models/song.dart';
+import 'package:xvibe_offline_mp3_player/pages/show_more_page.dart';
+import 'package:xvibe_offline_mp3_player/services/music_player_service.dart';
 import 'package:xvibe_offline_mp3_player/widgets/home/horizontal_text_and_text_button.dart';
 import '../../services/home/labeling_service.dart';
 import '../../widgets/home/vertical_song_card.dart';
+
+// TODO: Re-implement it into a responsive design
 
 class RoadTripSection extends StatefulWidget {
   const RoadTripSection({super.key});
@@ -13,6 +19,25 @@ class RoadTripSection extends StatefulWidget {
 }
 
 class _RoadTripSectionState extends State<RoadTripSection> {
+  final String playlistId = Playlistid.roadTrip;
+  final List<Song> songs = [
+    Song(id: 1, title: "J Tajor - All That Matters", vibe: "Chill", path: "assets/music/j_tajor.mp3"),
+    Song(id: 1, title: "Kehlani - Folded", vibe: "Chill", path: "assets/music/kehlani.mp3"),
+    Song(id: 1, title: "AllDay Project - Look at me", vibe: "Chill", path: "assets/music/adp.mp3"),
+  ];
+
+  List<AudioSource> get playlist => songs.map((song) 
+    => AudioSource.asset(song.path, tag: song)).toList();
+
+  @override
+  void initState() {
+    super.initState();
+    initPlaylist();
+  }
+
+  Future<void> initPlaylist() async =>
+    MusicPlayerService.setPlaylist(playlistId, playlist);
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -23,10 +48,13 @@ class _RoadTripSectionState extends State<RoadTripSection> {
           textButtonLabel: LabelName.showMore,
           callback: () {
             Navigator.push(
-              context, 
+              context,
               MaterialPageRoute(
-                builder: (_) => SongsPage(pageLabel: "Road Trip")
-              )
+                builder: (_) => ShowMorePage( 
+                  songs: songs, 
+                  playlistId: playlistId
+                ),
+              ),
             );
           },
         ),
@@ -39,12 +67,14 @@ class _RoadTripSectionState extends State<RoadTripSection> {
             scrollDirection: Axis.horizontal,
             itemBuilder: (_, index) {
               return VerticalSongCard(
-                songTitle: "My Music Card",
-                songVibe: "Road Trip",
+                songTitle: songs[index].title,
+                songVibe: songs[index].vibe,
+                playlistId: playlistId,
+                indexId: index,
               );
             },
             separatorBuilder: (_, _) => SizedBox(width: 8),
-            itemCount: 3,
+            itemCount: playlist.length,
           ),
         ),
       ],
