91783975f5448a43e1d55693f78d2bdf4569c875
Austine0829 <audreytuazon29@gmail.com>
2026-03-12 06:35:30 +0800

    (add) app database

---
+++ lib/data/application_database.dart
@@ -0,0 +1,41 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class ApplicationDatabase {
+  static final ApplicationDatabase instance = ApplicationDatabase._internal();
+
+  ApplicationDatabase._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    
+    _database = await _initDatabase();
+
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, "xvibe.db");
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute("""
+      CREATE TABLE song(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        vibe TEXT,
+        path TEXT,
+        isFavorite INTEGER
+      )
+    """);
+  }
+}
