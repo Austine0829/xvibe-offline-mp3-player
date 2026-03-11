ba4190f2e1278edf8fe9160adad2149c7ddf80b5
Austine0829 <audreytuazon29@gmail.com>
2026-03-12 06:21:32 +0800

    (add) playlist ids

---
+++ lib/constants/playlist_id.dart
@@ -0,0 +1,11 @@
+class Playlistid {
+  static const String roadTrip = "9f6c2b44-3a7e-4c9f-b2e1-8d5a6f7c1e3b";
+  static const String chaotic = "1f8a2b7d-6c4e-4f3b-9a21-5d8c7e9b2a64";
+  static const String recentTrack = "b4c3d1f9-8e7a-4a2c-93f6-1b5d8e2c7f40";
+  static const String chill = "7a6d5c1e-2f9b-4e3a-8c41-6d2b9f7e5a13";
+  static const String energetic = "c9e2a7f4-1d6b-4c8e-b532-9a7d3f6b2c10";
+  static const String acoustic = "3b7f1a9d-5c2e-4b6a-8d93-e1f4c7a2b658";
+  static const String topListen = "e3a9c6d1-5b7f-4a82-9c4e-1d6f2b8a7c35";
+  static const String mix = "4f2b8c7e-1a6d-4f93-a5c1-9e7b3d2c6a80";
+
+}
