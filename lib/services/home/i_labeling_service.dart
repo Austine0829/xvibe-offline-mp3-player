130dbfbf7e874bd1f1151b921b34377910c986ba
Austine0829 <audreytuazon29@gmail.com>
2026-03-12 21:53:35 +0800

    (update) labels

--- lib/services/home/i_labeling_service.dart
+++ lib/services/home/i_labeling_service.dart
@@ -5,7 +5,7 @@ import 'package:xvibe_offline_mp3_player/services/home/labeling_service.dart';
 abstract class ILabelingService {
   // ignore: unused_field
   late final Random _random;
-  late final Map<LabelType, List<String>> labels;
+  final Map<LabelType, List<String>> labels = {};
   
   String generate(LabelType type);
 }
