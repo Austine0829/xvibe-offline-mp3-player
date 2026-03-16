f72fcec7d3444a2c3f0ab5f2aee321261a7721aa
Austine0829 <audreytuazon29@gmail.com>
2026-03-16 23:56:53 +0800

    (add) const vibe values

---
+++ lib/constants/vibe.dart
@@ -0,0 +1,7 @@
+class Vibe {
+  static const String chill = "Chill";
+  static const String roadTrip = "Road Trip";
+  static const String chaotic = "Chaotic";
+  static const String energetic = "Energetic";
+  static const String acoustic  = "Acoustic";
+}
