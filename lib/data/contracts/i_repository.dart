035787eadb989890aacb0c453df7697534a6bac6
Austine0829 <audreytuazon29@gmail.com>
2026-03-16 23:55:33 +0800

    (update) add optional params to get all contract

--- lib/data/contracts/i_repository.dart
+++ lib/data/contracts/i_repository.dart
@@ -6,7 +6,7 @@ abstract class IRepository {
   late final Future<Database> _db;
 
   Future<void> add(Song song);
-  Future<List<Song>> getAll();
+  Future<List<Song>> getAll({String? vibe});
   Future<Song> get(int id);
   Future<void> update(int id, Song song);
   Future<void> delete(int id);
