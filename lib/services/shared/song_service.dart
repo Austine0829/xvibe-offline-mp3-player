0c18cbd90e48c8c457ce0c09b1b5112b529e622d
Austine0829 <audreytuazon29@gmail.com>
2026-03-24 00:49:01 +0800

    (add) song service

---
+++ lib/services/shared/song_service.dart
@@ -0,0 +1,37 @@
+import 'package:xvibe_offline_mp3_player/data/contracts/i_repository.dart';
+import 'package:xvibe_offline_mp3_player/models/song.dart';
+import 'package:xvibe_offline_mp3_player/services/shared/i_song_service.dart';
+
+class SongService implements ISongService {
+  late final IRepository _songRepository;
+
+  SongService(this._songRepository);
+  
+  @override
+  Future<void> addSong(Song song) async {
+    await _songRepository.add(song);
+  }
+
+  @override
+  Future<void> deletSong(int id) async {
+    await _songRepository.delete(id);
+  }
+
+  @override
+  Future<Song> getSong(int id) async {
+    Song song = await _songRepository.get(id);
+
+    return song;
+  }
+
+  @override
+  Future<List<Song>> getSongs({String? vibe}) async {
+    return await _songRepository.getAll(vibe: vibe);
+  }
+
+  @override
+  Future<void> updateSong(int id, Song song) async {
+    await _songRepository.update(id, song);
+  }
+
+}
