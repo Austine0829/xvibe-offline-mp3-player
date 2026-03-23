f8fac77dea11b99c09d92c054259882624bdf799
Austine0829 <audreytuazon29@gmail.com>
2026-03-24 00:45:48 +0800

    (add) view models contract

---
+++ lib/view models/i_vibe_view_model.dart
@@ -0,0 +1,13 @@
+import 'package:xvibe_offline_mp3_player/models/song.dart';
+
+abstract class IVibeViewModel {
+  List<Song> get getSongs;
+  String? get errorMessage;
+  bool get isLoading;
+
+  Future<void> play(int index);
+
+  Future<void> update(int id, Song song);
+  Future<void> initialize();
+  Future<void> delete(int id);
+}
