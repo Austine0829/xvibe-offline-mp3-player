16b139d361be9184b6ca364327e60939a342c403
Austine0829 <audreytuazon29@gmail.com>
2026-04-04 07:32:38 +0800

    (update) contract repo dependency

--- lib/services/shared/i_song_service.dart
+++ lib/services/shared/i_song_service.dart
@@ -1,10 +1,10 @@
-import 'package:xvibe_offline_mp3_player/data/contracts/i_repository.dart';
+import 'package:xvibe_offline_mp3_player/data/contracts/i_song_repository.dart';
 import 'package:xvibe_offline_mp3_player/models/song.dart';
 
 abstract class ISongService {
   
   // ignore: unused_field
-  late final IRepository _songRepository;
+  late final ISongRepository _songRepository;
 
   Future<void> addSong(Song song);
   Future<Song> getSong(int id);
