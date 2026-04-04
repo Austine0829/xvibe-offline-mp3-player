51f1303f8195b2d79de6d3ae5de6ffcae72b5190
Austine0829 <audreytuazon29@gmail.com>
2026-04-04 21:55:36 +0800

    (add) playlist add dialog

---
+++ lib/widgets/playlist/add_dialog.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:xvibe_offline_mp3_player/models/playlist.dart';
+import 'package:xvibe_offline_mp3_player/utils/random_color_picker.dart';
+import 'package:xvibe_offline_mp3_player/utils/uuid_generator.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/i_playlist_view_model.dart';
+
+class AddDialog extends StatefulWidget {
+  final IPlaylistViewModel playlistViewModel;
+
+  const AddDialog({
+    super.key,
+    required this.playlistViewModel,
+  });
+
+  @override
+  State<AddDialog> createState() => _AddDialogState();
+}
+
+class _AddDialogState extends State<AddDialog> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _name = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        "Add Playlist",
+        style: TextStyle(fontSize: 20)
+      ),
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: _name,
+              decoration: InputDecoration(
+                label: Text("Name",),
+                border: OutlineInputBorder()
+              ),
+              validator: (name) {
+                if (name == null || name.isEmpty) return "Please, put a name";
+
+                return null;
+              },
+            )
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("Close"),
+        ),
+        TextButton(
+          onPressed: () async {
+            final isValid = _formKey.currentState!.validate();
+
+            if (isValid) {
+              Navigator.pop(context);
+              await widget.playlistViewModel.add(
+                Playlist(
+                  id: UuidGenerator.generate(),
+                  name: _name.text, 
+                  backgroundColor: RandomColorPicker.generate()
+                )
+              );
+            }
+          },
+          child: Text("Add"),
+        ),
+      ],
+    );
+  }
+}
