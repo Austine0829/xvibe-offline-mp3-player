397e1b7b063c3c55d7647db529dd5f93afe511c7
Austine0829 <audreytuazon29@gmail.com>
2026-04-04 21:55:48 +0800

    (add) playlist edit dialog

---
+++ lib/widgets/playlist/edit_dialog.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xvibe_offline_mp3_player/models/playlist.dart';
+import 'package:xvibe_offline_mp3_player/services/shared/i_playlist_service.dart';
+import 'package:xvibe_offline_mp3_player/services/shared/playlist_service.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/i_playlist_view_model.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/playlist_view_model.dart';
+
+class EditDialog extends StatefulWidget {
+  final String playlistId;
+
+  const EditDialog({
+    super.key,
+    required this.playlistId,
+  });
+
+  @override
+  State<EditDialog> createState() => _EditDialogState();
+}
+
+class _EditDialogState extends State<EditDialog> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _name = TextEditingController();
+
+  late final IPlaylistService _playlistService;
+  bool isInitialize = false;
+  Playlist? _playlist;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (!isInitialize) {
+      _playlistService = context.read<PlaylistService>();
+      initialize(widget.playlistId);
+      isInitialize = true;
+    }
+  }
+
+  Future<void> initialize(String id) async {
+    _playlist = await _playlistService.getPlaylist(id);
+    setState(() {
+      _name.text = _playlist!.name;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final IPlaylistViewModel playlistViewModel = context.watch<PlaylistViewModel>();
+
+    return AlertDialog(
+      title: Text(
+        "Edit Playlist",
+        style: TextStyle(fontSize: 20)
+      ),
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: _name,
+              decoration: InputDecoration(
+                label: Text("Name",),
+                border: OutlineInputBorder()
+              ),
+              validator: (name) {
+                if (name == null || name.isEmpty) return "Please, put a name";
+
+                return null;
+              },
+            )
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("Close"),
+        ),
+        TextButton(
+          onPressed: () async {
+            final isValid = _formKey.currentState!.validate();
+
+            if (isValid) {
+              Navigator.pop(context);
+              final updatedPlaylist = _playlist!.copyWith(name: _name.text);
+              await playlistViewModel.update(widget.playlistId, updatedPlaylist);
+            }
+          },
+          child: Text("Update"),
+        ),
+      ],
+    );
+  }
+}
