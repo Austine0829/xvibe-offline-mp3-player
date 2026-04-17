0240654efe6d3768a2a6333246d54108e236e2d5
Austine0829 <audreytuazon29@gmail.com>
2026-04-17 23:56:57 +0800

    reworked and added new var

--- lib/widgets/playlist/playlist_card.dart
+++ lib/widgets/playlist/playlist_card.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:xvibe_offline_mp3_player/pages/playlist/playlist_songs_page.dart';
+import 'package:xvibe_offline_mp3_player/widgets/playlist/edit_playlist_dialog.dart';
 
 class PlaylistCard extends StatelessWidget {
-  final dynamic backgroundColor;
   final String textLabel;
-
+  final String playlistId;
+  final Color backgroundColor;
+  
   const PlaylistCard({
     super.key,
     required this.textLabel,
+    required this.playlistId,
     required this.backgroundColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: backgroundColor,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      child: Padding(
-        padding: EdgeInsets.only(top: 75),
-        child: ListTile(title: Text(textLabel)),
+      child: ListTile(
+        onTap: () {                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+          Navigator.push(
+            context, 
+            MaterialPageRoute(
+              builder: (_) => PlaylistSongsPage(playlistId: playlistId)
+            )
+          );
+        },
+        leading: Icon(Icons.library_music),
+        title: Text(textLabel),
+        tileColor: backgroundColor,
+        trailing: IconButton(onPressed: () {
+          showDialog(
+            context: context, 
+            builder: (context) {
+              return EditPlaylistDialog(playlistId: playlistId);
+            }                                                                                                                                     
+          );
+        }, icon: Icon(Icons.edit)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
     );
   }
