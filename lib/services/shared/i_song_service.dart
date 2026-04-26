477d395f37f4beedf52682ecb708635a29739e5d
Austine0829 <audreytuazon29@gmail.com>
2026-04-26 21:49:53 +0800

    add get method for audio sources cache, added a initalizer, song id fetcher method  and update params names

--- lib/services/shared/i_song_service.dart
+++ lib/services/shared/i_song_service.dart
@@ -1,14 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:xvibe_offline_mp3_player/DTO/song_dto.dart';
 import 'package:xvibe_offline_mp3_player/data/contracts/i_song_repository.dart';
 import 'package:xvibe_offline_mp3_player/models/song.dart';
 
-abstract class ISongService {
+abstract class ISongService extends ChangeNotifier {
   
   // ignore: unused_field
   late final ISongRepository _songRepository;
 
+  Map<int, AudioSource> get getAudioSources;
+
+  Map<int, Song> get getSongSources;
+
   Future<void> addSong(Song song);
-  Future<Song> getSong(int id);
+  Future<Song> getSong(int songId);
   Future<List<Song>> getSongs({String? vibe});
-  Future<void> updateSong(int id, Song song);
-  Future<void> deletSong(int id);
+  Future<void> updateSong(int songId, Song song);
+  Future<void> deletSong(int songId);
+  Future<void> initializeAudioSources();
+  Future<List<SongDTO>> getSongsId();
 }
--- lib/services/shared/song_service.dart
+++ lib/services/shared/song_service.dart
@@ -1,20 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:xvibe_offline_mp3_player/DTO/song_dto.dart';
 import 'package:xvibe_offline_mp3_player/data/contracts/i_song_repository.dart';
 import 'package:xvibe_offline_mp3_player/models/song.dart';
 import 'package:xvibe_offline_mp3_player/services/shared/i_song_service.dart';
 
-class SongService implements ISongService {
+class SongService extends ChangeNotifier implements ISongService {
   late final ISongRepository _songRepository;
+  final Map<int, AudioSource> _audioSources = {};
+  final Map<int, Song> _songSources = {};
 
   SongService(this._songRepository);
   
+  @override
+  Map<int, AudioSource> get getAudioSources => _audioSources;
+
+  @override
+  Map<int, Song> get getSongSources => _songSources;
+
   @override
   Future<void> addSong(Song song) async {
     await _songRepository.add(song);
   }
 
   @override
-  Future<void> deletSong(int id) async {
-    await _songRepository.delete(id);
+  Future<void> deletSong(int songId) async {
+    await _songRepository.delete(songId);
+
+    _audioSources.remove(songId);
+    _songSources.remove(songId);
+  
+    notifyListeners();
   }
 
   @override
@@ -30,8 +46,28 @@ class SongService implements ISongService {
   }
 
   @override
-  Future<void> updateSong(int id, Song song) async {
-    await _songRepository.update(id, song);
+  Future<void> updateSong(int songId, Song song) async {
+    await _songRepository.update(songId, song);
+
+    _audioSources[songId] = AudioSource.file(song.path, tag: song);
+    _songSources[songId] = song;
+
+    notifyListeners();
+  }
+  
+  @override
+  Future<void> initializeAudioSources() async {
+    final List<Song> songs = await _songRepository.getAll();
+
+    for (var song in songs) {
+      _audioSources[song.id] = AudioSource
+        .file(song.path, tag: song);
+      _songSources[song.id] = song;
+    }
   }
 
+  @override
+  Future<List<SongDTO>> getSongsId({String? vibe}) async {
+    return await _songRepository.getAllId(vibe: vibe);
+  }
 }
