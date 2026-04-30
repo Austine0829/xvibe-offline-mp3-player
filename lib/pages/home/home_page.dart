18c31f6963bc380d914a7b34f4d025751b792d93
Austine0829 <audreytuazon29@gmail.com>
2026-04-30 16:02:50 +0800

    move the home and show more page in home folder

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -30,7 +30,7 @@ class HomePage extends StatelessWidget {
               // TopListenSection(),
               // AcousticSection(),
               // ChaoticSection(),
-              // SizedBox(height: 50)
+              SizedBox(height: 100)
             ],
           ),
         ),
