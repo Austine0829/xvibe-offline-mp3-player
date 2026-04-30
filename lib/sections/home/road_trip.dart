abab26ee558aeb82f2e582399daca28f33066870
Austine0829 <audreytuazon29@gmail.com>
2026-04-30 16:11:32 +0800

    rework whole behavior of the component

--- lib/sections/home/road_trip.dart
+++ lib/sections/home/road_trip.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:provider/provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import 'package:xvibe_offline_mp3_player/constants/label_name.dart';
-import 'package:xvibe_offline_mp3_player/constants/playlist_id.dart';
 import 'package:xvibe_offline_mp3_player/models/song.dart';
-import 'package:xvibe_offline_mp3_player/pages/show_more_page.dart';
-import 'package:xvibe_offline_mp3_player/services/home/i_labeling_service.dart';
-import 'package:xvibe_offline_mp3_player/services/shared/i_music_player_service.dart';
-import 'package:xvibe_offline_mp3_player/services/shared/music_player_service.dart';
+import 'package:xvibe_offline_mp3_player/pages/home/show_more_page.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/road_trip_vibe_view_model.dart';
 import 'package:xvibe_offline_mp3_player/widgets/home/horizontal_text_and_text_button.dart';
-import '../../services/home/labeling_service.dart';
-import '../../widgets/home/vertical_song_card.dart';
+import 'package:xvibe_offline_mp3_player/widgets/shared/no_songs_found.dart';
+import '../../widgets/home/vibe_vertical_song_card.dart';
 
 class RoadTripSection extends StatefulWidget {
   const RoadTripSection({super.key});
@@ -20,77 +17,70 @@ class RoadTripSection extends StatefulWidget {
 }
 
 class _RoadTripSectionState extends State<RoadTripSection> {
-  late final IMusicPlayerService _musicPlayerService;
-  late final ILabelingService _labelingService;
-  final String playlistId = Playlistid.roadTrip;
-  final List<Song> songs = [
-    Song(id: 1, title: "J Tajor - All That Matters", vibe: "Chill", path: "assets/music/j_tajor.mp3"),
-    Song(id: 1, title: "Kehlani - Folded", vibe: "Chill", path: "assets/music/kehlani.mp3"),
-    Song(id: 1, title: "AllDay Project - Look at me", vibe: "Chill", path: "assets/music/adp.mp3"),
-  ];
-
-  List<AudioSource> get playlist => songs.map((song) 
-    => AudioSource.asset(song.path, tag: song)).toList();
-
-  bool isInitialize = false;
-
+  
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    if(!isInitialize) {
-      _labelingService = context.read<LabelingService>();
-      _musicPlayerService = context.read<MusicPlayerService>();
-      initPlaylist();
-      isInitialize = true;
-    }
+  void initState() {
+    super.initState();
+    final viewModel = context.read<RoadTripVibeViewModel>();
+    Future.microtask(() async => 
+      await viewModel.initialize());
   }
 
-  Future<void> initPlaylist() async =>
-    _musicPlayerService.setPlaylist(playlistId, playlist);
-
-  @override
+   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        HorizontalTextAndTextButton(
-          textLabel: _labelingService.generate(LabelType.roadTrip),
-          textButtonLabel: LabelName.showMore,
-          callback: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => ShowMorePage( 
-                  musicPlayerService: _musicPlayerService,
-                  songs: songs, 
-                  playlistId: playlistId
+    final RoadTripVibeViewModel roadTripVibeViewModel = context.watch<RoadTripVibeViewModel>();
+
+    return Skeletonizer(
+      enabled: roadTripVibeViewModel.isLoading,
+      child: Column(
+        children: [
+          if (roadTripVibeViewModel.getSongsId.isEmpty)
+            NoSongsFound(),
+          
+          if (roadTripVibeViewModel.getSongsId.isNotEmpty)
+            Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              HorizontalTextAndTextButton(
+                textLabel: roadTripVibeViewModel.generateLabel(),
+                textButtonLabel: LabelName.showMore,
+                callback: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => ShowMorePage(
+                        vibeViewModel: (context) => context.watch<RoadTripVibeViewModel>()
+                      ),
+                    ),
+                  );
+                },
+              ),
+              Container(
+                constraints: BoxConstraints(
+                  maxHeight: 200,
+                  maxWidth: double.infinity,
+                ),
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (_, index) {
+                    final int songId = roadTripVibeViewModel.getSongsId[index];
+                    final Song song = roadTripVibeViewModel.getSongs[songId]!;
+
+                    return VibeVerticalSongCard(
+                      vibeViewModel: roadTripVibeViewModel,
+                      songTitle: song.title,
+                      songVibe: song.vibe,
+                      indexId: index,
+                    );
+                  },
+                  separatorBuilder: (_, _) => SizedBox(width: 8),
+                  itemCount: roadTripVibeViewModel.getSongsId.length >= 10 ? 10 : roadTripVibeViewModel.getSongsId.length,
                 ),
               ),
-            );
-          },
-        ),
-        Container(
-          constraints: BoxConstraints(
-            maxHeight: 200,
-            maxWidth: double.infinity,
-          ),
-          child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (_, index) {
-              return VerticalSongCard(
-                musicPlayerService: _musicPlayerService,
-                songTitle: songs[index].title,
-                songVibe: songs[index].vibe,
-                playlistId: playlistId,
-                indexId: index,
-              );
-            },
-            separatorBuilder: (_, _) => SizedBox(width: 8),
-            itemCount: playlist.length,
-          ),
-        ),
-      ],
+            ],
+          )
+        ],
+      )
     );
   }
 }
