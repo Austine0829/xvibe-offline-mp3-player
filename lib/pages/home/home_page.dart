3b5437fb3b90c129b23249be62b289719b9a11e8
Austine0829 <audreytuazon29@gmail.com>
2026-04-30 16:25:24 +0800

    update recent track and roadtrip components path

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:xvibe_offline_mp3_player/sections/home/recent_tracks.dart';
-import '../sections/home/road_trip.dart';
-import '../utils/app_text_theme.dart';
+import 'sections/recent_tracks.dart';
+import 'sections/road_trip.dart';
+import '../../utils/app_text_theme.dart';
 
 // TODO: Remove the comment of other components after fixing the UI
 
