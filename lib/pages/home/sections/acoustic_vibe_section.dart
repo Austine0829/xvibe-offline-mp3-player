94feb5e39a13690c1eef4aa2ec1c4427c55423bc
Austine0829 <audreytuazon29@gmail.com>
2026-05-01 23:11:39 +0800

    rework the section and implement its function

---
+++ lib/pages/home/sections/acoustic_vibe_section.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+import 'package:xvibe_offline_mp3_player/constants/label_name.dart';
+import 'package:xvibe_offline_mp3_player/models/song.dart';
+import 'package:xvibe_offline_mp3_player/pages/home/show_more_page.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/acoustic_vibe_view_model.dart';
+import 'package:xvibe_offline_mp3_player/widgets/home/horizontal_text_and_text_button.dart';
+import 'package:xvibe_offline_mp3_player/widgets/shared/no_songs_found.dart';
+import '../../../widgets/home/vibe_vertical_song_card.dart';
+
+class AcousticVibeSection extends StatefulWidget {
+  const AcousticVibeSection({super.key});
+
+  @override
+  State<AcousticVibeSection> createState() => _AcousticVibeSectionState();
+}
+
+class _AcousticVibeSectionState extends State<AcousticVibeSection> {
+  late final String _label;
+
+  @override
+  void initState() {
+    super.initState();
+    final viewModel = context.read<AcousticVibeViewModel>();
+    Future.microtask(() async => 
+      await viewModel.initialize());
+    _label = viewModel.generateLabel();
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    final AcousticVibeViewModel acousticVibeViewModel = context.watch<AcousticVibeViewModel>();
+
+    return Skeletonizer(
+      enabled: acousticVibeViewModel.isLoading,
+      child: Column(
+        children: [
+          if (acousticVibeViewModel.getSongsId.isEmpty)
+            NoSongsFound(),
+          
+          if (acousticVibeViewModel.getSongsId.isNotEmpty)
+            Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              HorizontalTextAndTextButton(
+                textLabel: _label,
+                textButtonLabel: LabelName.showMore,
+                callback: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => ShowMorePage(
+                        vibeViewModel: (context) => context.watch<AcousticVibeViewModel>()
+                      ),
+                    ),
+                  );
+                },
+              ),
+              Container(
+                constraints: BoxConstraints(
+                  maxHeight: 200,
+                  maxWidth: double.infinity,
+                ),
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (_, index) {
+                    final int songId = acousticVibeViewModel.getSongsId[index];
+                    final Song song = acousticVibeViewModel.getSongs[songId]!;
+
+                    return VibeVerticalSongCard(
+                      vibeViewModel: acousticVibeViewModel,
+                      songTitle: song.title,
+                      songVibe: song.vibe,
+                      indexId: index,
+                    );
+                  },
+                  separatorBuilder: (_, _) => SizedBox(width: 8),
+                  itemCount: acousticVibeViewModel.getSongsId.length >= 10 ? 10 : acousticVibeViewModel.getSongsId.length,
+                ),
+              ),
+            ],
+          )
+        ],
+      )
+    );
+  }
+}
