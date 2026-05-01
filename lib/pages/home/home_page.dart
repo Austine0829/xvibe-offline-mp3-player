93cb57fd35cc53acce8d487a61f792c52c1dccae
Austine0829 <audreytuazon29@gmail.com>
2026-05-01 22:48:56 +0800

    add energetic, chiil, mix vibes back and added a home page watcher view model for refreshing iu with inits

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xvibe_offline_mp3_player/pages/home/sections/chill_vibe_section.dart';
+import 'package:xvibe_offline_mp3_player/pages/home/sections/mix_vibe_section.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/chill_vibe_view_model.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/energetic_vibe_view_model.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/home_page_view_model.dart';
+import 'package:xvibe_offline_mp3_player/pages/home/sections/energetic_section.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/i_home_page_view_model.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/recent_tracks_view_model.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/road_trip_vibe_view_model.dart';
 import 'sections/recent_tracks.dart';
 import 'sections/road_trip.dart';
 import '../../utils/app_text_theme.dart';
@@ -10,6 +20,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final IHomePageViewModel homePageViewModel = context.watch<HomePageViewModel>();
+
+    if (homePageViewModel.refresh() && context.mounted) {
+      // ignore: use_build_context_synchronously
+      Future.microtask(() async => await context.read<RoadTripVibeViewModel>().initialize());
+
+      // ignore: use_build_context_synchronously
+      Future.microtask(() async => await context.read<RecentTracksViewModel>().initialize());
+
+      // ignore: use_build_context_synchronously
+      Future.microtask(() async => await context.read<EnergeticVibeViewModel>().initialize());
+
+      // ignore: use_build_context_synchronously
+      Future.microtask(() async => await context.read<ChillVibeViewModel>().initialize());
+
+      homePageViewModel.setRefresh(false);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Home", style: Theme.of(context).textTheme.pageLabel),
@@ -24,13 +52,13 @@ class HomePage extends StatelessWidget {
               RoadTripSection(),
               SizedBox(height: 15),
               RecentTracksSection(),
-              // EnergeticSection(),
-              // ChillVibeSection(),
-              // MixSection(),
+              EnergeticSection(),
+              ChillVibeSection(),
+              MixVibeSection(),
               // TopListenSection(),
               // AcousticSection(),
               // ChaoticSection(),
-              SizedBox(height: 100)
+              SizedBox(height: 110)
             ],
           ),
         ),
