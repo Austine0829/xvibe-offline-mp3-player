bd09d17e8318170422ca5e2b06278c44f6b25d99
Austine0829 <audreytuazon29@gmail.com>
2026-05-02 23:22:58 +0800

    update gget and method names

--- lib/services/shared/i_song_log_service.dart
+++ lib/services/shared/i_song_log_service.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/foundation.dart';
 
 abstract class ISongLogService extends ChangeNotifier {
-  List<int> get getRecentTracksSongId;
-  List<int> get getTopListenTracksSongId;
+  List<int> get getRecentSongsId;
+  List<int> get getTopListenSongsId;
 
-  Future<List<int>> getRecentTracksSongIdByDate(String date);
+  Future<List<int>> getRecentSongsIdByDate(String date);
+  Future<List<int>> getTopListenSongsIdWithLimit({int limit = 25});
 }
