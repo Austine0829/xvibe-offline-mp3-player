3b301f7f83e0706d5469798cb76e7e69cb32f531
Austine0829 <audreytuazon29@gmail.com>
2026-05-02 22:21:12 +0800

    renamed file

--- lib/data/contracts/i_recent_track_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:xvibe_offline_mp3_player/models/recent_track.dart';
-
-abstract class IRecentTrackRepository {
-  Future<void> add(RecentTrack recentTrack);
-  Future<List<int>> getSongsId({required String date});
-  Future<List<RecentTrack>> getRecentTracks();
-}
