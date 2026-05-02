c8057bc2c6a2d7c2b15959da088c3d467af9b6c8
Austine0829 <audreytuazon29@gmail.com>
2026-05-02 23:14:05 +0800

    update getters name

--- lib/services/shared/i_song_log_service.dart
+++ lib/services/shared/i_song_log_service.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/foundation.dart';
 
 abstract class ISongLogService extends ChangeNotifier {
-  List<int> get getRecenTracksSongId;
+  List<int> get getRecentTracksSongId;
+  List<int> get getTopListenTracksSongId;
 
   Future<List<int>> getRecentTracksSongIdByDate(String date);
 }
