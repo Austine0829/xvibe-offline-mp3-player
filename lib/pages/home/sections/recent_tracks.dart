0d645bc00fbaccac99488b0e7e29949c2b29aaa0
Austine0829 <audreytuazon29@gmail.com>
2026-05-02 22:47:23 +0800

    update view model name

--- lib/pages/home/sections/recent_tracks.dart
+++ lib/pages/home/sections/recent_tracks.dart
@@ -4,8 +4,7 @@ import 'package:skeletonizer/skeletonizer.dart';
 import 'package:xvibe_offline_mp3_player/constants/label_name.dart';
 import 'package:xvibe_offline_mp3_player/models/song.dart';
 import 'package:xvibe_offline_mp3_player/pages/home/show_all_page.dart';
-import 'package:xvibe_offline_mp3_player/view%20models/i_recent_tracks_view_model.dart';
-import 'package:xvibe_offline_mp3_player/view%20models/recent_tracks_view_model.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/recent_log_songs_view_model.dart';
 import 'package:xvibe_offline_mp3_player/widgets/home/horizontal_text_and_text_button.dart';
 import 'package:xvibe_offline_mp3_player/widgets/home/recent_track_song_card.dart';
 
@@ -21,22 +20,22 @@ class _RecentTracksSectionState extends State<RecentTracksSection> {
   @override
   void initState() {
     super.initState();
-    final viewModel = context.read<RecentTracksViewModel>();
+    final viewModel = context.read<RecentLogSongsViewModel>();
     Future.microtask(() async => 
       await viewModel.initialize());
   }
 
   @override
   Widget build(BuildContext context) {
-    final IRecentTracksViewModel recentTracksViewModel = context.watch<RecentTracksViewModel>();
+    final RecentLogSongsViewModel recentLogSongsViewModel = context.watch<RecentLogSongsViewModel>();
 
     return Skeletonizer(
       enabled: false,
       child: Column(children: [
-        if (recentTracksViewModel.getRecentTracksSongId.isEmpty)
+        if (recentLogSongsViewModel.getRecentTracksSongId.isEmpty)
           SizedBox.shrink(),
 
-        if (recentTracksViewModel.getRecentTracksSongId.isNotEmpty) 
+        if (recentLogSongsViewModel.getRecentTracksSongId.isNotEmpty) 
           Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -48,7 +47,7 @@ class _RecentTracksSectionState extends State<RecentTracksSection> {
                   context,
                   MaterialPageRoute(
                     builder: (_) => ShowAllPage(
-                      recentTracksViewModel: context.watch<RecentTracksViewModel>()
+                      songLogViewModel: context.watch<RecentLogSongsViewModel>()
                     ),
                   ),
                 );
@@ -57,18 +56,18 @@ class _RecentTracksSectionState extends State<RecentTracksSection> {
             Column(
               children: [
                 ...List.generate(
-                  recentTracksViewModel.getRecentTracksSongId.length >= 5
-                  ? 5 : recentTracksViewModel.getRecentTracksSongId.length,
+                  recentLogSongsViewModel.getRecentTracksSongId.length >= 5
+                  ? 5 : recentLogSongsViewModel.getRecentTracksSongId.length,
                   (index) {
-                    final int songId = recentTracksViewModel.getRecentTracksSongId[index];
-                    final Song? song = recentTracksViewModel.getSongs[songId];
+                    final int songId = recentLogSongsViewModel.getRecentTracksSongId[index];
+                    final Song? song = recentLogSongsViewModel.getSongs[songId];
 
                     if (song == null) return SizedBox.shrink();
 
                     return Padding(
                       padding: EdgeInsets.only(bottom: 8),
                       child: RecentTrackSongCard(
-                        recentTracksViewModel: recentTracksViewModel, 
+                        songLogViewModel: recentLogSongsViewModel, 
                         songId: song.id, 
                         songTitle: song.title, 
                         songVibe: song.vibe, 
