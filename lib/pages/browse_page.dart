73eaee689a64c0b835f7c24e7973573a90d596fe
Austine0829 <audreytuazon29@gmail.com>
2026-05-05 23:19:45 +0800

    add functionalities and update ui logic

--- lib/pages/browse_page.dart
+++ lib/pages/browse_page.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:xvibe_offline_mp3_player/utils/app_text_theme.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/browse_vibe_view_model.dart';
 import 'package:xvibe_offline_mp3_player/widgets/browse/browse_catalog_grid_view.dart';
 import 'package:xvibe_offline_mp3_player/widgets/browse/browse_search_song_list_view.dart';
 
@@ -15,6 +16,8 @@ class BrowsePage extends StatefulWidget {
 class _BrowsePageState extends State<BrowsePage> {
   final ValueNotifier<String> _text = ValueNotifier('');
   Timer? _debounce;
+  final FocusNode _focusNode = FocusNode();
+  bool _isFocus= false;
 
   void debounce(String value) {
     if (_debounce?.isActive ?? false) _debounce!.cancel();
@@ -25,13 +28,26 @@ class _BrowsePageState extends State<BrowsePage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+        setState(() {
+          _isFocus = _focusNode.hasFocus;
+        });
+      }
+    );
+  }
+
+  @override
   void dispose() {
     _debounce?.cancel();
+    _focusNode.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
@@ -47,6 +63,7 @@ class _BrowsePageState extends State<BrowsePage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextField(
+                  focusNode: _focusNode,
                   onChanged: (value) => debounce(value),
                   cursorColor: Colors.black,
                   decoration: InputDecoration(
@@ -66,9 +83,13 @@ class _BrowsePageState extends State<BrowsePage> {
                 ValueListenableBuilder(
                   valueListenable: _text,
                   builder: (_, value, _) {
-                    return value.isEmpty
-                        ? const BrowseCatalogGridView()
-                        : const BrowseSearchSongListView();
+                    if (!_isFocus && value.isEmpty) return BrowseCatalogGridView();
+
+                    if (value.isEmpty || !context.mounted) return SizedBox.shrink();
+
+                    // ignore: use_build_context_synchronously
+                    Future.microtask(() async => context.read<BrowseVibeViewModel>().getSongsIdWithTitle(value));
+                    return BrowseSearchSongListView();
                   },
                 ),
               ],
