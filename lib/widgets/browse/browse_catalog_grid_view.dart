fc7ba30516a1e53c5ffcf8d50f47520bf6737de1
Austine0829 <audreytuazon29@gmail.com>
2026-05-05 22:59:53 +0800

    add functionalities

--- lib/widgets/browse/browse_catalog_grid_view.dart
+++ lib/widgets/browse/browse_catalog_grid_view.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/browse_vibe_view_model.dart';
+import 'package:xvibe_offline_mp3_player/view%20models/i_browse_vibe_view_model.dart';
 import 'package:xvibe_offline_mp3_player/widgets/browse/browse_catalog_grid_card.dart';
 import '../../utils/app_text_theme.dart';
 
 class BrowseCatalogGridView extends StatelessWidget {
+  static final List<Color> _colors = [
+    Colors.teal,
+    Colors.lightBlue,
+    Colors.deepOrangeAccent,
+    Colors.indigoAccent,
+    Colors.yellowAccent
+  ];
+
   const BrowseCatalogGridView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final IBrowseVibeViewModel browseVibeViewModel = context.watch<BrowseVibeViewModel>();
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: EdgeInsets.symmetric(vertical: 5),
           child: Text(
-            "Catalog",
+            "Catalogs",
             style: Theme.of(context).textTheme.sectionLabel,
           ),
         ),
@@ -24,10 +37,12 @@ class BrowseCatalogGridView extends StatelessWidget {
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
             childAspectRatio: 1.5,
-            children: List.generate(5, (index) {
+            children: List.generate(browseVibeViewModel.getVibes.length, (index) {
+
               return BrowseCatalogGridCard(
-                textLabel: "Road Trip",
-                color: Colors.lightBlue,
+                vibe: browseVibeViewModel.getVibes[index],
+                playlistId: browseVibeViewModel.getVibesPlaylistId[index],
+                color: _colors[index],
               );
             }),
           ),
