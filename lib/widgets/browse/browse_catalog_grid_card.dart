ca3336fb1a3bdf305c000db216495bf383fd4e06
Austine0829 <audreytuazon29@gmail.com>
2026-05-05 22:59:26 +0800

    add function and rework a bit

--- lib/widgets/browse/browse_catalog_grid_card.dart
+++ lib/widgets/browse/browse_catalog_grid_card.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:xvibe_offline_mp3_player/pages/browse/catalog_page.dart';
 
 class BrowseCatalogGridCard extends StatelessWidget {
-  final String textLabel;
-  final MaterialColor color;
+  final String vibe;
+  final String playlistId;
+  final Color color;
 
   const BrowseCatalogGridCard({
     super.key,
-    required this.textLabel,
+    required this.vibe,
+    required this.playlistId,
     required this.color,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Align(
-        alignment: AlignmentGeometry.bottomLeft,
-        child: Padding(
-          padding: EdgeInsets.all(12),
-          child: Text(textLabel, style: TextStyle(fontSize: 18)),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context, 
+          MaterialPageRoute(
+            builder: (context) => CatalogPage(
+              playlistId: playlistId,
+              vibe: vibe,
+            )
+          )
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Align(
+          alignment: AlignmentGeometry.bottomLeft,
+          child: Padding(
+            padding: EdgeInsets.all(12),
+            child: Text(vibe, style: TextStyle(fontSize: 18)),
+          ),
         ),
       ),
     );
