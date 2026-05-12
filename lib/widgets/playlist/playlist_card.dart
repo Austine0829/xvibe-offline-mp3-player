a901f788b324dd9b14f39a7f186bd8d6e2bf4af2
Austine0829 <audreytuazon29@gmail.com>
2026-05-12 23:19:02 +0800

    add smooth transition when going to playlist songs page

--- lib/widgets/playlist/playlist_card.dart
+++ lib/widgets/playlist/playlist_card.dart
@@ -6,7 +6,7 @@ class PlaylistCard extends StatelessWidget {
   final String textLabel;
   final String playlistId;
   final Color backgroundColor;
-  
+
   const PlaylistCard({
     super.key,
     required this.textLabel,
@@ -18,25 +18,46 @@ class PlaylistCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        onTap: () {                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+        onTap: () {
           Navigator.push(
-            context, 
-            MaterialPageRoute(
-              builder: (_) => PlaylistSongsPage(playlistId: playlistId)
-            )
+            context,
+            PageRouteBuilder(
+              pageBuilder: (_, _, _) => (
+                PlaylistSongsPage(playlistId: playlistId)
+              ),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                    final slide = Tween(
+                      begin: const Offset(1.0, 0.0),
+                      end: Offset.zero,
+                    ).chain(CurveTween(curve: Curves.easeOut));
+
+                    return FadeTransition(
+                      opacity: animation,
+                      child: SlideTransition(
+                        position: animation.drive(slide),
+                        child: child,
+                      ),
+                    );
+                  },
+              transitionDuration: const Duration(milliseconds: 800),
+            ),
           );
         },
         leading: Icon(Icons.library_music),
         title: Text(textLabel),
         tileColor: backgroundColor,
-        trailing: IconButton(onPressed: () {
-          showDialog(
-            context: context, 
-            builder: (context) {
-              return EditPlaylistDialog(playlistId: playlistId);
-            }                                                                                                                                     
-          );
-        }, icon: Icon(Icons.edit)),
+        trailing: IconButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return EditPlaylistDialog(playlistId: playlistId);
+              },
+            );
+          },
+          icon: Icon(Icons.edit),
+        ),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
     );
