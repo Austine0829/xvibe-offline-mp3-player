fc104be5c4dd0755842ea44e6ef69e8f8607b84f
Austine0829 <audreytuazon29@gmail.com>
2026-05-12 23:21:36 +0800

    add smooth transition when going to catalog page

--- lib/widgets/browse/browse_catalog_grid_card.dart
+++ lib/widgets/browse/browse_catalog_grid_card.dart
@@ -18,13 +18,29 @@ class BrowseCatalogGridCard extends StatelessWidget {
     return InkWell(
       onTap: () {
         Navigator.push(
-          context, 
-          MaterialPageRoute(
-            builder: (context) => CatalogPage(
-              playlistId: playlistId,
-              vibe: vibe,
-            )
-          )
+          context,
+          PageRouteBuilder(
+            pageBuilder: (_, _, _) => CatalogPage(
+              playlistId: playlistId, 
+              vibe: vibe
+            ),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+                  final slide = Tween(
+                    begin: const Offset(1.0, 0.0),
+                    end: Offset.zero,
+                  ).chain(CurveTween(curve: Curves.easeOut));
+
+                  return FadeTransition(
+                    opacity: animation,
+                    child: SlideTransition(
+                      position: animation.drive(slide),
+                      child: child,
+                    ),
+                  );
+                },
+            transitionDuration: const Duration(milliseconds: 800),
+          ),
         );
       },
       child: Container(
