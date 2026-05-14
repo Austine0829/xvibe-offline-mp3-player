ea27a620801c649b1a0107974a760a45b6f9dae9
Austine0829 <audreytuazon29@gmail.com>
2026-05-14 16:37:52 +0800

    add new keys

--- lib/constants/hive_keys.dart
+++ lib/constants/hive_keys.dart
@@ -1,3 +1,7 @@
 class HiveKeys {
   static final String queueCache = "a7f9c8d1-4b13-4e89-a2c3-9d10e8e52a4f"; 
+  static final String songsIdKey = "f3c8d4b1-7e6a-4d2f-9a5c-1b7e8c4f2d90";
+  static final String songIndexKey = "8a1f6c92-3d5b-4e71-b8c0-5f2d9a7e1c34";
+  static final String songSecondsPositionKey = "c7e2b5f9-1a4d-43c8-92fe-6d1b8a3f5c77";
+  static final String playlistIdKey = "2d9a7c4e-6f1b-4c82-a5d3-9e7f2b1c6a40";
 }
